7ff12e1cb89bc9ea71d8f9b33c629e3738de57ca
isuke <isuke770@gmail.com>
2024-02-23 01:32:48 +0900

    implement id

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
+fn add_bg(string: String, color: &str) -> String {
+  return format!("%K{{{}}} {} %k", color, string);
+}
+
+fn id() -> String {
+  let output = Command::new("id").arg("-un").output().expect("failed to execute process");
+  return String::from(String::from_utf8_lossy(&output.stdout).trim_end());
+}
+
 fn main() {
-  print!("Hello, Droolmaw $ ")
+  let id_prompt: String = add_bg(id(), "magenta");
+  print!("{}", id_prompt)
 }
