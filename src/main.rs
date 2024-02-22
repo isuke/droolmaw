871e8cba3e9f41c467b5452a055e4a8c39dfde8a
isuke <isuke770@gmail.com>
2024-02-23 02:44:32 +0900

    implement dir

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 
+const PATH_MAX_LENGTH: usize = 50;
+
 fn add_bg(string: String, color: &str) -> String {
   return format!("%K{{{}}} {} %k", color, string);
 }
@@ -9,7 +11,27 @@ fn id() -> String {
   return String::from(String::from_utf8_lossy(&output.stdout).trim_end());
 }
 
+fn dir() -> String {
+  let output = Command::new("pwd").output().expect("failed to execute process");
+  let stdout = String::from(String::from_utf8_lossy(&output.stdout).trim_end());
+
+  let collection: Vec<&str> = stdout.split("/").collect();
+  let current_dir = String::from(*collection.last().unwrap());
+  let path_of_other_than_current_dir = &collection[..collection.len()].join("/");
+
+  if path_of_other_than_current_dir.len() > PATH_MAX_LENGTH {
+    let path_of_other_than_current_dir = match path_of_other_than_current_dir.get(..PATH_MAX_LENGTH.min(path_of_other_than_current_dir.len())) {
+      Some(str) => str,
+      None => path_of_other_than_current_dir,
+    };
+    return format!("{}.../{}", path_of_other_than_current_dir, current_dir);
+  } else {
+    return format!("{}/{}", path_of_other_than_current_dir, current_dir);
+  }
+}
+
 fn main() {
   let id_prompt: String = add_bg(id(), "magenta");
-  print!("{}", id_prompt)
+  let dir_prompt: String = add_bg(dir(), "blue");
+  print!("{}{} ", id_prompt, dir_prompt)
 }
