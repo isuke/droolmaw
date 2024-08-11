a1ec5314bad2cb8ffc96c60610203e9baf98fa15
isuke <isuke770@gmail.com>
2024-08-11 12:24:05 +0900

    prepare RPROMPT

--- droolmaw.zsh-theme
+++ droolmaw.zsh-theme
@@ -1,5 +1,6 @@
 prompt_precmd() {
   PROMPT=`./target/release/droolmaw`
+  RPROMPT=`./target/release/droolmaw --right`
 }
 
 add-zsh-hook precmd prompt_precmd
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use std::env;
+
 mod l_prompt;
+mod r_prompt;
 mod segment;
 
 fn main() {
-  l_prompt::run()
+  let args: Vec<String> = env::args().collect();
+
+  if args[args.len() - 1] == "--right" {
+    r_prompt::run()
+  } else {
+    l_prompt::run()
+  }
 }
