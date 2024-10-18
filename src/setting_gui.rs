d75c51630c800b2f8707397dd73de5284115ef1b
isuke <isuke770@gmail.com>
2024-10-18 13:32:45 +0900

    [WIP]

---
+++ src/setting_gui.rs
@@ -0,0 +1,10 @@
+#[component]
+fn App() -> Element {
+  let mut count = use_signal(|| 0);
+
+  rsx! {
+      h1 { "High-Five counter: {count}" }
+      button { onclick: move |_| count += 1, "Up high!" }
+      button { onclick: move |_| count -= 1, "Down low!" }
+  }
+}
